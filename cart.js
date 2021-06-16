14862835f35e2c2f28b50c55e632597b57a7e669
Pratya0011 <pratya0011@gmail.com>
2021-06-16 21:33:52 +0530

    commit

--- cart.js
+++ cart.js
@@ -33,9 +33,9 @@ if(JSON.parse(localStorage.getItem("value")) === null){
         details.innerHTML += `<div id="product-name" class="p-name">${data.name}</div>
                               <div id="product-price" class="abc">x${data.no}</div> 
                               <div id="product-quantity">₹${(data.price*data.no).toFixed(2)} INR</div>
-                              <div><button class="button" onclick = 'addMore(${data.name})'>+</button></div>
-                              <div><button class="button" onclick = 'deleteMore(${data.name})'>-</button></div>
-                              <div><button id="button" onclick = "deleteItems(${data.name})">Remove</button></div>`   
+                              <div><button class="button" onclick = 'addMore(${data.id})'>+</button></div>
+                              <div><button class="button" onclick = 'deleteMore(${data.id})'>-</button></div>
+                              <div><button id="button" onclick = "deleteItems(${data.id})">Remove</button></div>`   
 
 
                               //display total amount
@@ -46,8 +46,9 @@ if(JSON.parse(localStorage.getItem("value")) === null){
 }
 //remove button
 function deleteItems(id){
+    
     var items = JSON.parse(localStorage.getItem("value"));
-    var newItem = items.filter((item)=>item.name !=id)
+    var newItem = items.filter((item)=>item.id !=id)
     localStorage.setItem("value",JSON.stringify(newItem))
     window.location.reload();
 }
@@ -66,11 +67,12 @@ function continueShopping(){
 }
 //plus button
 function addMore(id){
+    console.log(id)
     var item = []
     var values = JSON.parse(localStorage.getItem("value")) ;
     console.log(values)
     values.map(data=>{
-        if(id == data.name){
+        if(id == data.id){
             data.no+=1;
             
         }
@@ -85,7 +87,7 @@ function deleteMore(id){
     var values = JSON.parse(localStorage.getItem("value")) ;
     console.log(values)
     values.map(data=>{
-        if(id == data.name){
+        if(id == data.id){
             data.no<1?data.no = 1:'';
             data.no--;
             }
